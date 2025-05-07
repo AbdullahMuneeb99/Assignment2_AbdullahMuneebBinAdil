80020f0cd05c054f63f7d4b2d053f0edf85bf0d9
AbdullahMuneeb99 <abdullahmuneeb99@gmail.com>
2025-05-06 21:56:51 -0500

    created Item.java that stores character's strength and craft

---
+++ src/main/java/Item.java
@@ -0,0 +1,29 @@
+public abstract class Item {
+    private String name;
+    private int strength;
+    private int craft;
+
+    public Item(String name, int strength, int craft) {
+        this.name = name;
+        this.strength = strength;
+        this.craft = craft;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getCraft() {
+        return craft;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Str: " + strength + ", Craft: " + craft + ")";
+    }
+}
+
