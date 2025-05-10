5e3cf261d72aef4e8d611a4e97c7e2f68d57f7df
AbdullahMuneeb99 <abdullahmuneeb99@gmail.com>
2025-05-10 05:10:00 -0500

    Added Warrior and Wizard player character types using class extensions of character.

---
+++ src/main/java/Warrior.java
@@ -0,0 +1,5 @@
+public class Warrior extends Character {
+    public Warrior(String name) {
+        super(name, 10, 3, 30); // Example stats
+    }
+}
---
+++ src/main/java/Wizard.java
@@ -0,0 +1,5 @@
+public class Wizard extends Character {
+    public Wizard(String name) {
+        super(name, 3, 10, 25); // Example stats
+    }
+}
